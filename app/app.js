99df7eae395d464501923b78718e982e53da7978
07Akashh <aonecreations1@gmail.com>
2024-12-25 15:54:35 +0530

    feat: update app.js

--- app/app.js
+++ app/app.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT;
 connectDB();
 
 app.use(express.json());
-app.use(express.static('public'));
+app.use('/', express.static('public'));
 
 app.post('/run-script', trendsController.runScript);
 app.get('/latest', trendsController.getLatest);
